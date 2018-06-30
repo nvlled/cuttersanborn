f397943966d9ef110055606cf986e19ad31a210f
Ronald Casili <nvlled@gmail.com>
2018-06-30 11:39:28 +0800

    Rename cli script

--- cuttersanborn.js
+++ cuttersanborn.js
@@ -2,11 +2,13 @@
 
 async function main() {
     const process = require("process");
+    const {basename} = require("path");
+
     let generateCutter = await require("./lib")();
     let args = process.argv.slice(1);
     if (args.length < 3) {
         console.log("A Cutter-Sanborn Four-Figure number generator");
-        console.log(`usage: ${args[0]} <firstname> <lastname>`);
+        console.log(`usage: ${basename(args[0])} <firstname> <lastname>`);
         return;
     }
     let fname = args[1];
